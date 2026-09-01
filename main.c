073f3bddb0d856a1478d2d0cda96385dc346d019
Gingerbr3ad <rwiktor4627@gmail.com>
2026-09-01 16:19:51 +0200

    Plagiarsied uart driver to test if it even works

--- main.c
+++ main.c
@@ -1,23 +1,63 @@
- 
-#include <util/delay.h>
-#include <unistd.h>
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <joerg@FreeBSD.ORG> wrote this file.  As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return.        Joerg Wunsch
+ * ----------------------------------------------------------------------------
+ *
+ * Stdio demo
+ */
+
+#include "defines.h"
+
+#include <ctype.h>
+#include <stdint.h>
+#include <stdio.h>
+
 #include <avr/io.h>
+#include <avr/pgmspace.h>
 
-#define set_bit(reg, bit) (reg |= (1 << bit))
-#define clear_bit(reg, bit) (reg &= ~(1 << bit ))
-#define test_bit(reg, bit) (reg & (1 << bit))
-#define loop_until_bit_is_set(reg, bit) while(!test_bit(reg, bit))
-#define loop_until_bit_is_clear(reg, bit) while(test_bit(reg, bit))
+#include <util/delay.h>
 
-#define WAVE_PIN PB1
+#include "uart_driver.h"
 
-int main(void) {
-    set_bit(DDRB, WAVE_PIN);
-    
-    while(1) {
-        set_bit(PORTB, WAVE_PIN);
-        _delay_ms(500);
-        clear_bit(PORTB, WAVE_PIN);
-        _delay_ms(500);
+/*
+ * Do all the startup-time peripheral initializations.
+ */
+static void
+ioinit(void)
+{
+  uart_init();
+}
+
+FILE uart_str = FDEV_SETUP_STREAM(uart_putchar, uart_getchar, _FDEV_SETUP_RW);
+
+static void delay_1s(void)
+{
+  uint8_t i;
+
+  for (i = 0; i < 100; i++)
+    _delay_ms(10);
+}
+
+int main(void)
+{
+  uint8_t i;
+  char buf[20], s[20];
+
+  ioinit();
+
+  stdout = stdin = &uart_str;
+
+  fprintf(stderr, "Bye-bye");
+  delay_1s();
+  for (i = 0; i < 3; i++)
+    {
+      putc('.', stderr);
+      delay_1s();
     }
+  fprintf(stderr, "\n ");
+
+  return 0;
 }
---
+++ uart_driver.h
@@ -0,0 +1,18 @@
+void uart_init(void);
+
+/*
+ * Send one character to the UART.
+ */
+int uart_putchar(char c, FILE *stream);
+
+/*
+ * Size of internal line buffer used by uart_getchar().
+ */
+#define RX_BUFSIZE 80
+
+/*
+ * Receive one character from the UART.  The actual reception is
+ * line-buffered, and one character is returned from the buffer at
+ * each invocation.
+ */
+int uart_getchar(FILE *stream);
