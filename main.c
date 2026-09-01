5d5345b5449290004abecd754b6b678276e8c0ba
Gingerbr3ad <rwiktor4627@gmail.com>
2026-09-01 14:24:07 +0200

    1st programming test

---
+++ main.c
@@ -0,0 +1,23 @@
+ 
+#include <stdint.h>
+#include <unistd.h>
+#include <avr/io.h>
+
+#define set_bit(reg, bit) (reg |= (1 << bit))
+#define clear_bit(reg, bit) (reg &= ~(1 << bit ))
+#define test_bit(reg, bit) (reg & (1 << bit))
+#define loop_until_bit_is_set(reg, bit) while(!test_bit(reg, bit))
+#define loop_until_bit_is_clear(reg, bit) while(test_bit(reg, bit))
+
+#define WAVE_PIN PB1
+
+int main(void) {
+    set_bit(DDRB, WAVE_PIN);
+    
+    while(1) {
+        set_bit(PORTB, WAVE_PIN);
+        sleep(1);
+        clear_bit(PORTB, WAVE_PIN);
+        sleep(1);
+    }
+}
