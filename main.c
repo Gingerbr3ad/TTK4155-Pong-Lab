cfa21ca038f2b63f520b26fa2fe8714452ecb9b0
Gingerbr3ad <rwiktor4627@gmail.com>
2026-09-02 10:30:22 +0200

    fix

--- main.c
+++ main.c
@@ -1,63 +1,22 @@
-/*
- * ----------------------------------------------------------------------------
- * "THE BEER-WARE LICENSE" (Revision 42):
- * <joerg@FreeBSD.ORG> wrote this file.  As long as you retain this notice you
- * can do whatever you want with this stuff. If we meet some day, and you think
- * this stuff is worth it, you can buy me a beer in return.        Joerg Wunsch
- * ----------------------------------------------------------------------------
- *
- * Stdio demo
- */
-
-#include "defines.h"
-
-#include <ctype.h>
-#include <stdint.h>
-#include <stdio.h>
-
-#include <avr/io.h>
-#include <avr/pgmspace.h>
-
+ 
 #include <util/delay.h>
+#include <unistd.h>
+#include <avr/io.h>
 
-#include "uart_driver.h"
 
-/*
- * Do all the startup-time peripheral initializations.
- */
-static void
-ioinit(void)
-{
-  uart_init();
-}
+#define set_bit(reg, bit) (reg |= (1 << bit))
+#define clear_bit(reg, bit) (reg &= ~(1 << bit ))
+#define test_bit(reg, bit) (reg & (1 << bit))
 
-FILE uart_str = FDEV_SETUP_STREAM(uart_putchar, uart_getchar, _FDEV_SETUP_RW);
+#define WAVE_PIN PB1
 
-static void delay_1s(void)
-{
-  uint8_t i;
-
-  for (i = 0; i < 100; i++)
-    _delay_ms(10);
-}
-
-int main(void)
-{
-  uint8_t i;
-  char buf[20], s[20];
-
-  ioinit();
-
-  stdout = stdin = &uart_str;
-
-  fprintf(stderr, "Bye-bye");
-  delay_1s();
-  for (i = 0; i < 3; i++)
-    {
-      putc('.', stderr);
-      delay_1s();
+int main(void) {
+    set_bit(DDRB, WAVE_PIN);
+    
+    while(1) {
+        set_bit(PORTB, WAVE_PIN);
+        _delay_ms(500);
+        clear_bit(PORTB, WAVE_PIN);
+        _delay_ms(500);
     }
-  fprintf(stderr, "\n ");
-
-  return 0;
 }
