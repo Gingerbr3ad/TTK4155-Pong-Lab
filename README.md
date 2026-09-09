350c7692518bea9830a084d807bddf178d11b762
Gingerbr3ad <rwiktor4627@gmail.com>
2026-09-09 10:25:28 +0200

    Functional xmem testet with latch, added readme

---
+++ README.md
@@ -0,0 +1,4 @@
+Desk 9 - Gruppe 22 - Set Bygger'n 43
+PC password - Sanntid15
+
+https://ttk4155.github.io/docs/
--- main.c
+++ main.c
@@ -31,11 +31,14 @@ int main(void) {
     external_memory_init();
     
     while(1) { 
+        int address = 4096;
+        while(address < 4351) {
+            unsigned char *p = (unsigned char *) address;
+            *p = 0xaa;
+            ++address;
+            _delay_ms(80);
+        }
         
-        unsigned char *p = (unsigned char *) 0x100f;
-        _delay_ms(1000);
-        *p = 0x10f0;
-        _delay_ms(1000);
 
         //char c = getchar(); // Waits until it gets a character on the stdin stream
         //printf("Hello, PC! You've sent me this: %c\n", c); 
